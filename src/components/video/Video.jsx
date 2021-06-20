5170cf34a36e3ec267af99bfef6341f8c66a5eb6
kumard3 <kumardeepanshu157200@gmail.com>
2021-06-20 07:41:42 -0700

    updated

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,76 +1,80 @@
- import React, { useState } from 'react'
-import './_video.scss'
+import React, { useState,useEffect } from "react";
+import "./_video.scss";
+import { AiFillEye } from "react-icons/ai";
+import request from "../../api";
+import moment from "moment";
+import numeral from "numeral";
+const Video = ({ video }) => {
+  const {
+    id,
+    snippet: {
+      channelId,
+      channelTitle,
+      title,
+      publishedAt,
+      thumbnails: { medium },
+    },
+  } = video;
 
-import { AiFillEye } from 'react-icons/ai'
-import request from '../../api'
-import moment from 'moment'
-import numeral from 'numeral'
-const Video = ({video}) => {
+  const [views, setViews] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [channelIcon, setChannelIcon] = useState(null);
 
-   const { id, snippet: {channelID,channelTitle,title,publishedAt, thumbnails:{medium}} } = video
-
-
-   const [views,setViews] = useState(null)
-   const [duration,setDuration] = useState(null)
-   const [channelIcon,setChannelIcon] = useState(null)
-
-   const seconds = moment.duration(duration).asSeconds()
+  const seconds = moment.duration(duration).asSeconds()
    const _duration = moment.utc(seconds * 1000).format('mm:ss')
 
-   React.useEffect(() => {
-      const get_video_details = async() => {
-         const { data: {items}  } = await request ('/videos', {
-            prams: {
+   const _videoId = id?.videoId || id
+
+   useEffect(() => {
+      const get_video_details = async () => {
+         const {
+            data: { items },
+         } = await request('/videos', {
+            params: {
                part: 'contentDetails,statistics',
-               id:id,
+               id: _videoId,
             },
          })
          setDuration(items[0].contentDetails.duration)
          setViews(items[0].statistics.viewCount)
       }
       get_video_details()
-   },[id])
+   }, [_videoId])
 
-   React.useEffect(() => {
-      const get_channel_icons = async() => {
-         const { data: {items}  } = await request ('/channels', {
-            prams: {
-               part: 'snippet ',
-               id: channelID,
+   useEffect(() => {
+      const get_channel_icon = async () => {
+         const {
+            data: { items },
+         } = await request('/channels', {
+            params: {
+               part: 'snippet',
+               id: channelId,
             },
          })
          setChannelIcon(items[0].snippet.thumbnails.default)
       }
-      get_channel_icons()
-   },[channelID])
+      get_channel_icon()
+   }, [channelId])
 
-   return (
-      <div className='video'>
-         <div className='video__top'>
-            <img
-               src={medium.url}
-               alt=''
-            />
-            <span>{_duration}</span>
-         </div>
-         <div className='video__title'>
-           {title}
-         </div>
-         <div className='video__details'>
-            <span>
-               <AiFillEye /> {numeral(views).format("0.a")} Views •
-            </span>
-            <span> {moment(publishedAt).fromNow()} </span>
-         </div>
-         <div className='video__channel'>
-            <img
-               src={channelIcon?.url}
-               alt=''
-            />
-            <p>{channelTitle} </p>
-         </div>
+  return (
+    <div className="video">
+      <div className="video__top">
+        <img src={medium.url} alt="" />
+        <span>{_duration}</span>
       </div>
-   )
-}
+      <div className="video__title">{title}</div>
+      <div className="video__details">
+        <span>
+          <AiFillEye /> {numeral(views).format("0.a")} Views •{" "}
+        </span>
+        <span> {moment(publishedAt).fromNow()} </span>
+      </div>
+      <div className="video__channel">
+        <img src={channelIcon?.url} alt="" />
+        <p>{channelTitle} </p>
+      </div>
+    </div>
+  );
+};
 
-export default Video
+export default Video;
